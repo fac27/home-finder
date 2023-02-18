eeeab3a806f6b11cfe46b5900b102405d076c2da
Zakaria Yaris <info@zakariya.me>
2023-02-18 18:46:03 +0000

    add public transport info data
    Relates #20

---
+++ ui/displayTransportInfo.js
@@ -0,0 +1,31 @@
+const transportContainer = document.querySelector('#output__transport');
+
+export const displayTransportInfo = (buses) => {
+  // bus info section
+  const busesDiv = document.createElement('div');
+  busesDiv.className = 'buses__div';
+
+  const busesDivHeader = document.createElement('h3');
+  busesDivHeader.className = 'buses__div-header';
+  busesDivHeader.textContent = 'Nearest bus stops';
+
+  for (let busStop of buses) {
+    const busStopName = document.createElement('p');
+    busStopName.textContent = `${busStop.commonName}  Stop: ${
+      busStop.indicator
+    }   Distance: ${Math.round(busStop.distance)}`;
+
+    const busesNames = document.createElement('p');
+    busesNames.textContent = 'Buses:';
+
+    for (let bus of busStop.lines) {
+      busesNames.append(` ${bus.name}`);
+    }
+
+    busesDiv.append(busStopName);
+    busesDiv.append(busesNames);
+  }
+
+  transportContainer.innerHTML = '';
+  transportContainer.append(busesDiv);
+};
---
+++ utils/getNearestTransport.js
@@ -0,0 +1,43 @@
+// return info about the nearest public transport access points within a specified radius.
+// Works only for London as the data is from Tfl
+import { getLongAndLat } from './getLongAndLat.js';
+
+export const getNearestBuses = async (postcode) => {
+  const { latitude, longitude } = await getLongAndLat(postcode);
+
+  try {
+    const response = await fetch(
+      `https://api.tfl.gov.uk/Stoppoint?lat=${latitude}&lon=${longitude}&stoptypes=NaptanBusCoachStation,NaptanPublicBusCoachTram&radius=300&useStopPointHierarchy=True&returnLines=True`
+    );
+
+    if (response) {
+      const responseJson = await response.json();
+      return responseJson.stopPoints;
+    } else {
+      throw new Error("Couldn'nt get transport information");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// method to return nearby railway and underground stations
+// rsponse time has a bit of a delay
+// export const getNearestTrains = async (postcode) => {
+//   const { latitude, longitude } = await getLongAndLat(postcode);
+
+//   try {
+//     const response = await fetch(
+//       `https://api.tfl.gov.uk/Stoppoint?lat=${latitude}&lon=${longitude}&stoptypes=NaptanMetroStation,NaptanRailStation,&radius=3000&useStopPointHierarchy=False&returnLines=False`
+//     );
+
+//     if (response) {
+//       const responseJson = await response.json();
+//       return responseJson.stopPoints;
+//     } else {
+//       throw new Error("Couldn'nt get transport information");
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
